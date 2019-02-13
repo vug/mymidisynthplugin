d6380b695d07a8aa1933da4a796d3659c35f5ea8
Ugur Guney <ugurguney@gmail.com>
2019-02-13 01:01:54 -0500

    Use current processor variable values when initializing Editor UI values.

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,73 +9,73 @@ MyMidiSynthPlugInAudioProcessorEditor::MyMidiSynthPlugInAudioProcessorEditor (My
 	osc1TypeSelect.addItem("Sinusoidal", (int)oscillatorTypes::sinusoidal);
 	osc1TypeSelect.addItem("Saw Tooth", (int)oscillatorTypes::sawtooth);
 	osc1TypeSelect.addItem("Square", (int)oscillatorTypes::square);
-
 	osc1TypeSelect.onChange = [this] { 
 		processor.osc1.type = (oscillatorTypes)osc1TypeSelect.getSelectedId(); 
 	};
-	osc1TypeSelect.setSelectedId((int)oscillatorTypes::sinusoidal);
+	osc1TypeSelect.setSelectedId((int)processor.osc1.type);
 
 	addAndMakeVisible(osc2TypeSelect);
 	osc2TypeSelect.addItem("Sinusoidal", (int)oscillatorTypes::sinusoidal);
 	osc2TypeSelect.addItem("Saw Tooth", (int)oscillatorTypes::sawtooth);
 	osc2TypeSelect.addItem("Square", (int)oscillatorTypes::square);
-
-	osc2TypeSelect.onChange = [this] { processor.osc2.type = (oscillatorTypes)osc2TypeSelect.getSelectedId(); };
-	osc2TypeSelect.setSelectedId((int)oscillatorTypes::sinusoidal);
+	osc2TypeSelect.onChange = [this] { 
+		processor.osc2.type = (oscillatorTypes)osc2TypeSelect.getSelectedId(); 
+	};
+	osc2TypeSelect.setSelectedId((int)processor.osc2.type);
 
 	addAndMakeVisible(shiftSemitonesKnob);
 	shiftSemitonesKnob.setSliderStyle(Slider::Rotary);
 	shiftSemitonesKnob.setRotaryParameters(1.5 * float_Pi, 2.5 * float_Pi, false);
 	shiftSemitonesKnob.setRange(-12, 12, 1.0);
 	shiftSemitonesKnob.setTextValueSuffix(" semitones");
-	shiftSemitonesKnob.setValue(0);
 	shiftSemitonesKnob.setTextBoxStyle(Slider::NoTextBox, false, 60, 20);
 	shiftSemitonesKnob.setPopupDisplayEnabled(true, true, this, 2000);
 	shiftSemitonesKnob.onValueChange = [this] {
 		processor.osc2.freqShiftSemitones = shiftSemitonesKnob.getValue();
 	};
+	shiftSemitonesKnob.setValue(processor.osc2.freqShiftSemitones);
 
 	addAndMakeVisible(shiftCentsKnob);
 	shiftCentsKnob.setSliderStyle(Slider::Rotary);
 	shiftCentsKnob.setRotaryParameters(1.5 * float_Pi, 2.5 * float_Pi, false);
 	shiftCentsKnob.setRange(-100, 100, 1.0);
 	shiftCentsKnob.setTextValueSuffix(" cents");
-	shiftCentsKnob.setValue(0);
 	shiftCentsKnob.setTextBoxStyle(Slider::NoTextBox, false, 60, 20);
 	shiftCentsKnob.setPopupDisplayEnabled(true, true, this, 2000);
 	shiftCentsKnob.onValueChange = [this] {
 		processor.osc2.freqShiftCents = shiftCentsKnob.getValue();
 	};
+	shiftCentsKnob.setValue(processor.osc2.freqShiftCents);
 
 	addAndMakeVisible(oscMixSlider);
 	oscMixSlider.setSliderStyle(Slider::LinearHorizontal);
 	oscMixSlider.setRange(0.0, 1.0, 0.1);
-	oscMixSlider.setValue(0.0);
 	oscMixSlider.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
 	oscMixSlider.setPopupDisplayEnabled(true, true, this, 2000);
 	oscMixSlider.onValueChange = [this] {
 		processor.oscVolumesMix = oscMixSlider.getValue();
 	};
+	oscMixSlider.setValue(processor.oscVolumesMix);
 
 	addAndMakeVisible(envAttackSlider);
 	envAttackSlider.setSliderStyle(Slider::LinearVertical);
 	envAttackSlider.setRange(0.01, 2.0, 0.01);
-	envAttackSlider.setValue(0.01);
 	envAttackSlider.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
 	envAttackSlider.setPopupDisplayEnabled(true, true, this, 2000);
 	envAttackSlider.onValueChange = [this] {
 		processor.volArEnv.setParameters(getVolumeEnvelopeParameters());
 	};
+	envAttackSlider.setValue(processor.volArEnv.getParameters().attack);
 
 	addAndMakeVisible(envReleaseSlider);
 	envReleaseSlider.setSliderStyle(Slider::LinearVertical);
 	envReleaseSlider.setRange(0.01, 2.0, 0.01);
-	envReleaseSlider.setValue(0.01);
 	envReleaseSlider.setTextBoxStyle(Slider::NoTextBox, false, 0, 0);
 	envReleaseSlider.setPopupDisplayEnabled(true, true, this, 2000);
 	envReleaseSlider.onValueChange = [this] {
 		processor.volArEnv.setParameters(getVolumeEnvelopeParameters());
 	};
+	envReleaseSlider.setValue(processor.volArEnv.getParameters().release);
 
     setSize (400, 300);
 }
