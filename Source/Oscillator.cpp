3504205fe8f060382dcb76eceb059b9acfb85034
Ugur Guney <ugurguney@gmail.com>
2019-02-13 21:47:39 -0500

    Set x initially to 0 because we are going to add multiple harmonics.

--- Source/Oscillator.cpp
+++ Source/Oscillator.cpp
@@ -18,7 +18,7 @@ Oscillator::~Oscillator()
 
 double Oscillator::oscillate()
 {
-	double x;
+	double x = 0.0;
 	double freqMultiplier = pow(2.0, freqShiftSemitones / 12.0 + freqShiftCents / 1200.0);
 	double f = frequency * freqMultiplier;
 	switch (type) {
