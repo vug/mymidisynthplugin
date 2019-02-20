8f696e157af64d7efe384d9d4b1f26e73cab9180
Ugur Guney <ugurguney@gmail.com>
2019-02-20 00:20:07 -0500

    Initialize empty synth voice class.

--- Source/MySynthesizerVoice.cpp
+++ Source/MySynthesizerVoice.cpp
@@ -1,11 +1,29 @@
-/*
-  ==============================================================================
-
-    MySynthesizerVoice.cpp
-    Created: 19 Feb 2019 11:56:20pm
-    Author:  veliu
-
-  ==============================================================================
-*/
-
 #include "MySynthesizerVoice.h"
+
+MySynthesizerVoice::MySynthesizerVoice() {
+
+}
+
+MySynthesizerVoice::~MySynthesizerVoice() {
+
+}
+
+bool MySynthesizerVoice::canPlaySound(SynthesiserSound* sound) {
+	return dynamic_cast<MySynthesizerSound*> (sound) != nullptr;
+}
+
+void MySynthesizerVoice::startNote(
+	int midiNoteNumber,
+	float velocity,
+	SynthesiserSound* sound,
+	int currentPitchWheelPosition) {
+
+}
+
+void MySynthesizerVoice::stopNote(float velocity, bool allowTailOff) {
+
+}
+
+void MySynthesizerVoice::renderNextBlock(AudioBuffer<double>& outputBuffer, int startSample, int numSamples) {
+
+}
--- Source/MySynthesizerVoice.h
+++ Source/MySynthesizerVoice.h
@@ -1,11 +1,35 @@
-/*
-  ==============================================================================
-
-    MySynthesizerVoice.h
-    Created: 19 Feb 2019 11:56:20pm
-    Author:  veliu
-
-  ==============================================================================
-*/
-
 #pragma once
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "MySynthesizerSound.h"
+
+class MySynthesizerVoice : public SynthesiserVoice {
+public:
+	MySynthesizerVoice();
+	~MySynthesizerVoice();
+
+	bool canPlaySound(SynthesiserSound*) override;
+
+	void startNote(int midiNoteNumber,
+		float velocity,
+		SynthesiserSound* sound,
+		int currentPitchWheelPosition) override;
+
+	void stopNote(float velocity, bool allowTailOff) override;
+
+	void renderNextBlock(AudioBuffer<double>& outputBuffer,
+		int startSample,
+		int numSamples) override;
+
+	//void renderNextBlock(AudioBuffer<float>& outputBuffer,
+	//	int startSample,
+	//	int numSamples) override;
+
+	//bool isVoiceActive() const;
+	//void pitchWheelMoved(int newPitchWheelValue) override;
+	//void controllerMoved(int controllerNumber, int newControllerValue) override;
+	//void aftertouchChanged(int newAftertouchValue);
+	//void channelPressureChanged(int newChannelPressureValue) override;
+
+	//bool isPlayingChannel(int midiChannel) const;
+private:
+};
